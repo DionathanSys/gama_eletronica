c4df067934c0dd89df45ddf94dcf10f37a6291c5
Dionathan Silva <dionideev@gmail.com>
2024-11-10 16:31:18 -0300

    Teste com powergrid

--- routes/web.php
+++ routes/web.php
@@ -6,19 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    $imp = (new ImpostoServico())->create([
-        'codigo_municipio' => 123456,
-        'aliq_iss' => 3.00,
-    ]);
-
-    $serv = (new Servico())->create([
-        'nome' => 'Teste',
-        'descricao' => 'Teste Desc',
-        'imposto_servico_id' => 1,
-    ]);
-
-    
-
-    dd($serv->impostos);
-
+    return view('welcome');
 });
