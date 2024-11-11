e34c07bada5c0212f124d944d7005d9c3e12a8f6
Dionathan Silva <dionideev@gmail.com>
2024-11-11 05:52:46 -0300

    WIP: feat(Fatura): criado relacionamento com parceiro

--- app/Models/Fatura.php
+++ app/Models/Fatura.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Fatura extends Model
 {
-    //
+     /*
+    |--------------------------------------------------------------------------
+    | Relacionamentos
+    |--------------------------------------------------------------------------
+    */
+
+    public function parceiro(): HasOne
+    {
+        return $this->hasOne(Parceiro::class);
+    }
 }
