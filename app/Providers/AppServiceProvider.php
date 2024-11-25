4cb6beb9fc60436fee6ef3a1b2b88597322c46c4
Dionathan Silva <dionideev@gmail.com>
2024-11-24 23:17:41 -0300

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
 
         Browsershot::html('Foo')
             // ->setChromePath('/usr/bin/chromium-browser')
+            ->noSandbox()
             ->setNodeBinary(env('AMBIENTE') == 'windows' ? 'C:\Program Files\nodejs\node.exe' : '/usr/bin/node') 
             ->setNpmBinary(env('AMBIENTE') == 'windows' ? 'C:\Program Files\nodejs\npm.cmd' : '/usr/bin/npm');
 
