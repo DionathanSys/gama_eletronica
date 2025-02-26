6d8088cf26550504018933708c77614b4afe978e
Dionathan Silva <dionideev@gmail.com>
2025-02-25 22:08:45 -0300

    inclusão de nova natureza em Enum

--- app/Enums/NaturezaOperacaoEnum.php
+++ app/Enums/NaturezaOperacaoEnum.php
@@ -4,11 +4,12 @@ namespace App\Enums;
 
 enum NaturezaOperacaoEnum:string
 {
-    case VENDA_PRODUTO = 'VENDA DE PRODUTO';
-    case VENDA_SERVICO = 'VENDA DE SERVIÇO';
-    case REMESSA_CONSIGNACAO = 'REMESSA DE MERCADORIA OU BEM PARA CONSERTO OU REPARO';
-    case DEVOLUCAO_VENDA = 'DEVOLUÇÃO DE VENDA';
-    case RETORNO_MERCADORIA = 'RETORNO DE MERCADORIA';
+    case VENDA_PRODUTO                  = 'VENDA DE PRODUTO';
+    case VENDA_SERVICO                  = 'VENDA DE SERVIÇO';
+    case REMESSA_CONSIGNACAO            = 'REMESSA DE MERCADORIA OU BEM PARA CONSERTO OU REPARO';
+    case DEVOLUCAO_VENDA                = 'DEVOLUÇÃO DE VENDA';
+    case RETORNO_MERCADORIA             = 'RETORNO DE MERCADORIA';
+    case ESTORNO_NFE_NAO_CANCELADA      = 'ESTORNO NFE NÃO CANCELADA NO PRAZO LEGAL';
 
     /**
      * Obter todas as opções como um array associativo
@@ -28,11 +29,11 @@ enum NaturezaOperacaoEnum:string
     public function description(): string
     {
         return match ($this) {
-            self::VENDA_PRODUTO => 'OPERAÇÃO DE VENDA DE PRODUTOS.',
-            self::VENDA_SERVICO => 'PRESTAÇÃO DE SERVIÇOS.',
-            self::REMESSA_CONSIGNACAO => 'REMESSA DE MERCADORIA OU BEM PARA CONSERTO OU REPARO',
-            self::DEVOLUCAO_VENDA => 'DEVOLUÇÃO DE PRODUTOS PELO CLIENTE.',
-            self::RETORNO_MERCADORIA => 'RETORNO DE MERCADORIA',
+            self::VENDA_PRODUTO         => 'OPERAÇÃO DE VENDA DE PRODUTOS.',
+            self::VENDA_SERVICO         => 'PRESTAÇÃO DE SERVIÇOS.',
+            self::REMESSA_CONSIGNACAO   => 'REMESSA DE MERCADORIA OU BEM PARA CONSERTO OU REPARO',
+            self::DEVOLUCAO_VENDA       => 'DEVOLUÇÃO DE PRODUTOS PELO CLIENTE.',
+            self::RETORNO_MERCADORIA    => 'RETORNO DE MERCADORIA',
         };
     }
 }
