8d39118590c1989a980ed866da0df3f6242a19fc
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-05-14 14:04:54 -0300

    ajuste tamanho do conteudo na tela

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -9,6 +9,7 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
+use Filament\Support\Enums\MaxWidth;
 use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
@@ -25,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->topNavigation()
             ->sidebarCollapsibleOnDesktop()
+            ->maxContentWidth(MaxWidth::Full)
             ->brandName('Gamma Eletrônica')
             ->databaseNotifications()
             ->default()
