468ea9cadc27e77052dd04a83f720af22af205af
Dionathan Vieira Rodrigues da Silva <dionathan.silva@magna.local>
2025-07-04 11:55:56 -0300

    fix

--- app/Traits/DefineCfop.php
+++ app/Traits/DefineCfop.php
@@ -11,7 +11,7 @@ trait DefineCfop
     public static function getCfop(Parceiro $parceiro, string $tipo_nota): int
     {
         $operacao = $parceiro->endereco->estado == 'SC' ? 'intraestadual' : 'interestadual';
-
+        dd(config("nfe.cfop.{$operacao}.{$tipo_nota}"));
         return config("nfe.cfop.{$operacao}.{$tipo_nota}");
     }
 
